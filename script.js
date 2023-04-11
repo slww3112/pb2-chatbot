f5fae88c3561882c032dc2964890ab1316f99c5a
Muhammed <xdankiiv3@gmail.com>
2023-04-11 13:45:12 +0200

    Some answers

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const firebaseConfig = {
     measurementId: "G-25LXV3YTB7"
 };
 
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.classList.toggle("dark-mode");
+});
 const darkModeToggle = document.getElementById("toggle-dark-mode");
 
 darkModeToggle.addEventListener("click", () => {
@@ -48,28 +51,71 @@ function addMessageToChat(sender, message) {
     chatOutput.scrollTop = chatOutput.scrollHeight;
 }
 
+function generateChatbotResponse(message) {
+    message = message.toLowerCase();
+    let response = '';
 
+    if (message.includes('hello') || message.includes('hi')) {
+        response = 'Hello! How can I help you today?';
+    } else if (message.includes('how are you')) {
+        response = "I'm doing well, thank you! How can I help you?";
+    } else if (message.includes('what is your name')) {
+        response = "I'm a simple chatbot. How can I assist you?";
+    } else if (message.includes('thank you')) {
+        response = "You're welcome! Let me know if you have any more questions.";
+    } else {
+        response = "I'm not sure how to respond to that. Can you please rephrase your question?";
+    }
+
+    return response;
+}
+
+function getFormattedDateAndTime() {
+    const now = new Date();
+    const timeZone = 'Etc/GMT-2'; // Set the timezone to GMT+2
+
+    const dateFormatter = new Intl.DateTimeFormat('en-GB', {
+        timeZone,
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+    });
+
+    const timeFormatter = new Intl.DateTimeFormat('en-GB', {
+        timeZone,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+    });
+
+    const formattedDate = dateFormatter.format(now);
+    const formattedTime = timeFormatter.format(now);
+
+    return `${formattedDate} ${formattedTime}`;
+}
 async function processUserMessage(message) {
     const messageRef = firebase.database().ref("messages");
     const newMessageRef = messageRef.push();
     await newMessageRef.set({
         sender: "User",
         message: message,
-        timestamp: Date.now(),
+        timestamp: getFormattedDateAndTime(),
     });
 
-    // bsp chatbot response
-    const response = `You said: ${message}`; // Placeholder response
+    // Use the generateChatbotResponse function to get a response
+    const response = generateChatbotResponse(message);
 
     const newResponseRef = messageRef.push();
     await newResponseRef.set({
         sender: "Chatbot",
         message: response,
-        timestamp: Date.now(),
+        timestamp: getFormattedDateAndTime(),
     });
 
     addMessageToChat("Chatbot", response);
 }
+
 /*
 function loadChatHistory() {
     const messagesRef = firebase.database().ref("messages");
